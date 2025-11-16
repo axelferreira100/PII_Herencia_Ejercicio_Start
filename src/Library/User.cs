6195d2eb4259ad4fea93e1d4bf859cea985ce90c
axelferreira100 <GUSTAVO.FERREIRA@correo.ucu.edu.uy>
2025-11-16 19:10:30 -0300

    Created User class.

---
+++ src/Library/User.cs
@@ -0,0 +1,32 @@
+namespace Library
+{
+    public abstract class User
+    {
+        protected string name, lastName, id;
+        protected int calification;
+
+        public string Name
+        {
+            get { return this.name; }
+            protected set { this.name = value; }
+        }
+
+        public string LastName
+        {
+            get { return this.lastName; }
+            protected set { this.lastName = value; }
+        }
+
+        public string Id
+        {
+            get { return this.id; }
+            protected set { this.id = value; }
+        }
+
+        public int Calification
+        {
+            get { return this.calification; }
+            protected set { this.calification = value; }
+        }
+    }
+}
