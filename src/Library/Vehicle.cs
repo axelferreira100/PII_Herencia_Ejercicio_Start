a6c2f1362c7b6c251d0cc53e2baa0bf3a6b7fc14
axelferreira100 <GUSTAVO.FERREIRA@correo.ucu.edu.uy>
2025-11-16 22:00:21 -0300

    Fixed Vehicle class.

--- src/Library/Vehicle.cs
+++ src/Library/Vehicle.cs
@@ -2,38 +2,38 @@ namespace Library
 {
     public abstract class Vehicle
     {
-        private string _mark, _model, _color;
-        private double _speed = 0.0;
-        private bool _status = false;
+        protected string _mark, _model, _color;
+        protected double _speed = 0.0;
+        protected bool _status = false;
 
         public string Mark
         {
             get { return this._mark; }
-            private set { this._mark = value; }
+            protected set { this._mark = value; }
         }
 
         public string Model
         {
             get { return this._model; }
-            private set { this._model = value; }
+            protected set { this._model = value; }
         }
 
         public string Color
         {
             get { return this._color; }
-            private set { this._color = value; }
+            protected set { this._color = value; }
         }
 
         public double Speed
         {
             get { return this._speed; }
-            private set { this._speed = value; }
+            protected set { this._speed = value; }
         }
 
         public bool Status
         {
             get { return this._status; }
-            private set { this._status = value; }
+            protected set { this._status = value; }
         }
 
         public Vehicle(string mark, string model, string color)
